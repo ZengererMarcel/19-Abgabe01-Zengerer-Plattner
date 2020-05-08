2db77681089730c4baa6fb483a966846407ca620
Tiffany Plattner <tiffany.plattner@edu.fh-joanneum.at>
2020-05-08 16:23:54 +0200

    workspace.xml
    merge conflict

---
+++ Drinks/src/test/java/at/fhj/iit/SimpleDrinkTest.java
@@ -0,0 +1,70 @@
+package at.fhj.iit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Class which tests the methods of SimpleDrink
+ */
+@DisplayName("Testing SimpleDrink implementation")
+public class SimpleDrinkTest {
+    private Liquid nonA, alcoholic;
+
+    /**
+     * inits an alcoholic and a nonalcoholic simple drink for EACH test
+     */
+    @BeforeEach
+    void setup() {
+
+    }
+
+    @Test
+    @DisplayName("Testing constructor non-alcoholic")
+    public void testConstructorNonAlcoholic(){
+
+    }
+    @Test
+    @DisplayName("Testing constructor alcoholic")
+    public void testConstructorAlcoholic(){
+
+    }
+
+    @Test
+    @DisplayName("Testing getVolume() non-aloholic")
+    public  void testGetVolumeNonAlcoholic(){
+
+    }
+
+    @Test
+    @DisplayName("Testing getVolume() alcoholic")
+    public void testGetVolumeAlcoholic(){
+
+    }
+
+    @Test
+    @DisplayName("Testing getAlcoholPercent non-alcoholic")
+    public void testGetAlcoholPercentNonAlcoholic(){
+
+    }
+    @Test
+    @DisplayName("Testing getAlcoholPercent alcoholic")
+    public void testGetAlcoholPercentAlcoholic(){
+
+    }
+
+    @Test
+    @DisplayName("Testing isAlcoholic non-alcoholic")
+    public void testIsAlcoholicNonAlcoholic(){
+
+    }
+    @Test
+    @DisplayName("Testing isAlcoholic alcoholic")
+    public void testIsAlcoholicAlcoholic(){
+
+    }
+
+}
