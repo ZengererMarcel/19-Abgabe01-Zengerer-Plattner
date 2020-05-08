f8156edb7cfd65d960be6f8eb36f1a328bbe5073
Marcel <marcel.zengerer@edu.fh-joanneum.at>
2020-05-08 17:07:51 +0200

    MixedDrinkTest: testConstructorAlcoholic()
    Die Vergleiche in testConstructorAlcoholic() implementiert

--- Drinks/src/test/java/at/fhj/iit/MixedDrinkTest.java
+++ Drinks/src/test/java/at/fhj/iit/MixedDrinkTest.java
@@ -21,27 +21,34 @@ public class MixedDrinkTest {
     void setup() {
         nonA = new MixedDrink("Spezi", new Liquid[]{new Liquid("Cola", 0.5, 0), new Liquid("Fanta", 0.5, 0)});
         alcoholic = new MixedDrink("Rum-Cola", new Liquid[]{new Liquid("Cola", 0.5, 0), new Liquid("Rum", 0.05, 37.5)});
-        nonA = new MixedDrink("Spezi", new Liquid[]{new Liquid("Cola", 0.5, 0), new Liquid("Rum", 0.05, -7)});
+        negativeA = new MixedDrink("Rum-Cola", new Liquid[]{new Liquid("Cola", 0.5, 0), new Liquid("Rum", 0.05, -7)});
     }
 
     @Test
     @DisplayName("Testing constructor non alcoholic")
     public void testConstructorNonAlcoholic(){
-        Liquid[] l = new Liquid[]{new Liquid("Cola", 0.5, 0), new Liquid("Fanta", 0.5, 0)};
-        assertEquals(nonA.name, "Spezi");
+            assertEquals(nonA.name, "Spezi");
 
-        assertEquals(nonA.liquidArray[0].getName(), "Cola");
-        assertEquals(nonA.liquidArray[0].getVolume(), 0.5);
-        assertEquals(nonA.liquidArray[0].getAlcoholPercent(), 0.0);
+            assertEquals(alcoholic.liquidArray[0].getName(), "Cola");
+            assertEquals(alcoholic.liquidArray[0].getVolume(), 0.5);
+            assertEquals(alcoholic.liquidArray[0].getAlcoholPercent(), 0.0);
 
-        assertEquals(nonA.liquidArray[1].getName(), "Fanta");
-        assertEquals(nonA.liquidArray[1].getVolume(), 0.5);
-        assertEquals(nonA.liquidArray[1].getAlcoholPercent(), 0.0);
+            assertEquals(alcoholic.liquidArray[1].getName(), "Fanta");
+            assertEquals(alcoholic.liquidArray[1].getVolume(), 0.5);
+            assertEquals(alcoholic.liquidArray[1].getAlcoholPercent(), 0.0);
     }
     @Test
     @DisplayName("Testing constructor alcoholic")
     public void testConstructorAlcoholic(){
+        assertEquals(alcoholic.name, "Rum-Cola");
 
+        assertEquals(alcoholic.liquidArray[0].getName(), "Cola");
+        assertEquals(alcoholic.liquidArray[0].getVolume(), 0.5);
+        assertEquals(alcoholic.liquidArray[0].getAlcoholPercent(), 0.0);
+
+        assertEquals(alcoholic.liquidArray[1].getName(), "Rum");
+        assertEquals(alcoholic.liquidArray[1].getVolume(), 0.05);
+        assertEquals(alcoholic.liquidArray[1].getAlcoholPercent(), 37.5);
     }
     @Test
     @DisplayName("Testing constructor negative alcoholic")
