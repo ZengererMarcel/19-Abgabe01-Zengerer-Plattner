3afadd1d545cfb646a4f02a423f82f2255dec7cf
Marcel <marcel.zengerer@edu.fh-joanneum.at>
2020-05-08 14:16:06 +0200

    MixedDrink Klasse erstellt
    Die MixedDrink Klasse erstellt, Methode hinzugefügt und mittels JavaDoc dokumentiert

---
+++ Drinks/src/main/java/at/fhj/iit/MixedDrink.java
@@ -0,0 +1,56 @@
+package at.fhj.iit;
+
+/**
+ * Class represents a mixed drink liquid which can be used in drinks
+ */
+public class MixedDrink extends Drink{
+
+    /**
+     * mixed drinks contains more than one liquid
+     */
+    Liquid[] liquidArray;
+
+    /**
+     * Creates a MixedDrink Object with given name and Liquid Array
+     *
+     * @param name  name of drink
+     * @param liquidArray contains the driffrent liquids for the mixed drink
+     */
+    public MixedDrink(String name, Liquid[] liquidArray) {
+        super(name);
+        this.liquidArray = liquidArray;
+    }
+
+    /**
+     * Returns volume of the mixed drink
+     *
+     * @return the volume of drink in litre
+     */
+    @Override
+    public double getVolume() {
+        //TODO
+        return 1;
+    }
+
+    /**
+     * Returns alcohol volume percent of mixed drink
+     *
+     * @return alcohol volume percent
+     */
+    @Override
+    public double getAlcoholPercent() {
+        //TODO
+        return 1;
+    }
+
+    /**
+     * Gives information if drink is alcoholic or not
+     *
+     * @return true when alcoholic liquids are present, otherwise false
+     */
+    @Override
+    public boolean isAlcoholic() {
+        //TODO
+        return true;
+    }
+}
